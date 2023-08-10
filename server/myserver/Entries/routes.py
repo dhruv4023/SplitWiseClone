02cdd52a06167eaed732c8c400793ba6130387ec
dhruv <dhruv20345@gmail.com>
2023-08-10 22:45:18 +0530

    entries route done

--- server/myserver/Entries/routes.py
+++ server/myserver/Entries/routes.py
@@ -4,9 +4,9 @@ from myserver.Entries.controller import *
 
 
 urlpatterns = [
-    path('add/<str:u_id>', add_entries, name="addentries"),
-    path('delete/<str:u_id>', delete_entries, name="deleteentries"),
-    path('get/<str:u_id>/<str:startIndex>', get_entries, name="getentries"),
+    path('add/<str:uid>/<str:gid>', add_entries, name="addentries"),
+    path('delete/<str:uid>/<str:gid>/<str:eid>', delete_entries, name="deleteentries"),
+    path('get/<str:gid>/<str:startIndex>/<str:limit>', get_entries, name="getentries"),
 ]
 
 
