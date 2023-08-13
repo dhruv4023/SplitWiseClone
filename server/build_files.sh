4ddada4c2b73f2a4da168e7f733d4be670a8d3f6
dhruv <dhruv20345@gmail.com>
2023-08-13 09:58:32 +0530

    version changed in req.txt

--- server/build_files.sh
+++ server/build_files.sh
@@ -1,6 +1,5 @@
 
 echo "BUID START"
-python -V
-pip install -r requirements.txt
-python manage.py collectstatic 
+python3.9 pip install -r requirements.txt
+python3.9 manage.py collectstatic 
 echo "BUID END" 
