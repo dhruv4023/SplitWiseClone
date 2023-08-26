867671c39fd5aa6eb40ffabe0165cbf5153733d2
dhruv <dhruv20345@gmail.com>
2023-08-26 23:58:24 +0530

    test

--- server/RunApp.ps1
+++ server/RunApp.ps1
@@ -8,10 +8,9 @@ if (-not $venvExists) {
     # Create virtual environment if it doesn't exist
     Write-Host "Creating virtual environment..."
     python -m venv $venvName
-    
-    Write-Host "Installing dependencies..."
-    python -m pip install -r "requirements.txt"
 }
+Write-Host "Installing dependencies..."
+python -m pip install -r "requirements.txt"
 try {
     & venv\Scripts\Activate.ps1
 
